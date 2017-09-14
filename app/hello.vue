fea77a7ebd4f7519ee5cf2ba625a85ebc097fdad
Your Name <you@example.com>
2017-09-15 01:00:23 +0300

    add v-if into template

--- app/hello.vue
+++ app/hello.vue
@@ -1,11 +1,15 @@
 <template>
-    <h1>{{msg}}</h1>
+    <div>
+        <h1>{{msg}}</h1>
+        <h2 v-if="show">It' should be displayed</h2>
+    </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        show: true
       }
     }
   }
@@ -13,5 +17,8 @@
 <style>
     h1 {
         color: green;
+    }
+    h2 {
+        color: blue;
     }
 </style>
